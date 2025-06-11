e0f204869ba27660542f2edbcd3786b521d334eb
Ron-10 <roneykarki011@gmail.com>
2025-06-11 12:25:41 +0545

    Configure for custom domain deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
+// vite.config.ts
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/portfolio/', // <-- Add this line
+  base: '/', // <-- CORRECTED for custom domain
   plugins: [react()],
 })
-```
