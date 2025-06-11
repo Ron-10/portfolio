b044dd2ab3c8f46af60b32ad0b0ea70e5e7fe4b0
Ron-10 <roneykarki011@gmail.com>
2025-06-11 22:49:52 +0545

    updated values

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
+import path from "path"
 
 export default defineConfig({
   plugins: [react()],
   base: "/portfolio", // This should be correct from our last fix
-
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
