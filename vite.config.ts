9008801a9401f4a6eabd0155e04f5a3781785488
Ron-10 <roneykarki011@gmail.com>
2025-06-12 15:42:25 +0545

    updated values

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,22 @@
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
-import path from "path"
 
-export default defineConfig({
-  plugins: [react()],
-  base: "/portfolio", // This should be correct from our last fix
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { componentTagger } from "lovable-tagger";
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => ({
+  base: "/",
+  server: {
+    host: "::",
+    port: 8080,
+  },
+  plugins: [react(), mode === "development" && componentTagger()].filter(
+    Boolean
+  ),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+}));
